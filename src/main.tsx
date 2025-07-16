5dd3ecdd8e3965623c96532b4ad68ee5d4bb7073
Rishabh Jain <76813738+rjain198@users.noreply.github.com>
2025-07-16 18:53:46 +0530

    fix issues

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import ErrorBoundary from './components/ErrorBoundary.jsx'
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 );
